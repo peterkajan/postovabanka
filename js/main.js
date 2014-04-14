e17a85af7de810075722e8d51f152e383f9f3a4d
Peter Kajan <peto.kajan@gmail.com>
2014-04-14 07:56:33 +0200

    htc - initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 /* custom javascript */
+var focusTime = 800;
+
+$( function() {
+	$('.image2').css('opacity', 0);
+	$('#button2').hide();
+	
+	$('#button1').click( function(){
+		toggleFocus();
+		$(this).hide();
+		$('#button2').show();
+	});
+	$('#button2').click( function(){
+		toggleFocus();
+		$(this).hide();
+		$('#button1').show();
+	});	
+});
+
+function toggleFocus() {
+	var img1El = $('.image1');
+	var img2El = $('.image2');
+	opac1 = img1El.css("opacity");
+	opac2 = img2El.css("opacity");
+	$('.image1').fadeTo(focusTime, 1 - opac1);
+	$('.image2').fadeTo(focusTime, 1 - opac2);
+}
