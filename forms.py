8abd9533ffac508a821050a616e3ba650f4d9e41
Peter Kajan <peto.kajan@gmail.com>
2014-04-25 21:54:14 +0200

    Revert "htc - initial commit"
    This reverts commit e17a85af7de810075722e8d51f152e383f9f3a4d.
    
    Conflicts:
    	page1.html
    	stylesheets/main.css

--- forms.py
+++ forms.py
@@ -4,3 +4,9 @@ from django.forms.fields import CharField
 
 class Page1Form(Form):
     error_css_class = 'error'
+    id_num = CharField()
+    
+    
+    def clean_id_num(self):
+        if self.cleaned_data['id_num'] == 'error':
+            raise ValidationError('Nespravne cislo')
